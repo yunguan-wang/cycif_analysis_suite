32cfdc5e741c99e22c6bd94a3b31881b85432092
Yunguan 'Jake' Wang <jake_y_wang@ymail.com>
2019-02-07 12:28:37 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
-from setuptools import setup
+from setuptools import setup,find_packages
+import versioneer
+
+requires = [
+    'numpy>=1.15.1',
+    'scipy>=1.0.1',
+    'pandas>=0.23.0',
+    'scikit-learn>=0.20.1',
+    'matplotlib',
+    'seaborn>=0.9.0',
+    'statsmodels',
+    'fuzzywuzzy',
+    'synapseclient',
+]
+VERSION = versioneer.get_version()
+DESCRIPTION = ('A comprehensive collection of tools for analysing segmented Cycif data.')
 
 setup(name='cycifsuite',
-      version='0.1',
-      description='A comprehensive collection of tools for analysing segmented Cycif data.',
+      version=VERSION,
+      description=DESCRIPTION,
       url='https://github.com/yunguan-wang/cycif_analysis_suite',
       author='Yunguan Wang',
       author_email='yunguan_wang@hms.harvard.edu',
       license='MIT',
-      packages=['cycifsuite'],
-      install_requires=[
-          'pandas',
-          'numpy',
-          'scipy',
-          'scikit-learn',
-          'matplotlib',
-          'seaborn',
-          'statsmodels',
-          'fuzzywuzzy',
-          'synapseclient'
-      ],
+      packages=find_packages(),
+      install_requires=requires,
       zip_safe=False)
